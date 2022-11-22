23e8e7ccd376d23c6136241b66255b82f5c851b8
Alexandr <artamoshkinas@gmail.com>
2022-11-22 20:28:50 +0700

    Задание 1 выполнено!

--- Home_Work_1.py
+++ Home_Work_1.py
@@ -1,18 +1,26 @@
-def weekend_search(day: int, days_of_the_week={1: 'Monday',
-                                               2: 'Tuesday',
-                                               3: 'Wednesday',
-                                               4: 'Thursday',
-                                               5: 'Friday',
-                                               6: 'Saturday',
-                                               7: 'Sunday'}):
+def weekend_search(days_of_the_week={1: 'Понедельник',
+                                     2: 'Вторник',
+                                     3: 'Среда',
+                                     4: 'Четверг',
+                                     5: 'Пятница',
+                                     6: 'Суббота',
+                                     7: 'Воскресенье'}):
     try:
         day = int(input('Введите день недели от 1 до 7: '))
         if 1 <= day <= 5:
-            print(f'{day} это {days_of_the_week.values(day)}')
-        print(f'{day} это {days_of_the_week.values(day)}')
+            print(f'Печаль! {days_of_the_week[day]}. К сожелению это рабочий день ')
+        else:
+            print(f'Ура! {days_of_the_week[day]}! Выходной!')
     except ValueError:
         print('Введенные данные не соответствуют входным!\n'
               'Пожалуйста повторите ввод.')
+        print('-' * 40)
+        return weekend_search()
+    except  KeyError:
+        print('Дней в недели только 7!\n'
+              'Пожалуйста повторите ввод.')
+        print('-' * 40)
         return weekend_search()
 
 
+weekend_search()
